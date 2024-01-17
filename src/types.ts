6d16014984300d2869e0f9515eef7b60994a92ad
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-17 17:12:13 +0100

    Add BuildCellParams, ScreenStoreState, GridStoreState types

--- src/types.ts
+++ src/types.ts
@@ -5,9 +5,7 @@ type Cell = {
   isAlive: boolean;
 };
 
-type GridType = {
-  grid: Cell[][];
-};
+type GridType = Cell[][];
 
 type ClockState = {
   play: boolean;
@@ -24,4 +22,31 @@ type ClockState = {
   addSpeed: () => void;
   subSpeed: () => void;
   queueTicks: (ticks: number) => void;
+};
+type BuildCellParams =
+  | [mode: "random", x: number, y: number, width: number]
+  | [mode: "inherit", x: number, y: number, width: number, isAlive: boolean];
+type ScreenStoreState = {
+  width: number;
+  height: number;
+  nRow: () => number;
+  nCol: () => number;
+  nCell: () => number;
+  updateScreen: () => void;
+};
+
+type GridStoreState = {
+  grid: GridType;
+  generation: number;
+  build: (mode: BuildCellMode) => GridType;
+  draw: () => void;
+  nextGen: () => void;
+  countAliveNeighbors: (row: number, col: number) => number;
+  judgement: (cell: Cell, neighbors: number) => boolean;
+  nextCycle: () => void;
+};
+type BuildCellMode = "random" | "inherit";
+
+type CanvasProps = {
+  screen: ScreenStoreState;
 };
