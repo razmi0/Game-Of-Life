31fcfea4f77ceabcbf2ba557f3cc0ee47b836b98
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-17 01:52:18 +0100

    Update vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -12,9 +12,9 @@ export default defineConfig({
     solidPlugin(),
   ],
   server: {
-    port: 3000,
+    port: 42069,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
