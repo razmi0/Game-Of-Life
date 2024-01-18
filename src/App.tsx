d19e51e48d408b8fcfcb3b789ff0265fa04ca82d
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-18 15:17:41 +0100

    Add Drawer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Canvas from "./components/Canvas";
 import useScreen from "./useScreen";
 import type { Component } from "solid-js";
+import Drawer from "./components/Drawer";
 
 const App: Component = () => {
   const screen = useScreen();
-  return <Canvas screen={screen} />;
+  return (
+    <>
+      <Drawer />
+      <Canvas screen={screen} />
+    </>
+  );
 };
 
 export default App;
