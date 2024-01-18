3e36df2db81241c402bba6f21f10bfdcb962b88c
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-18 20:42:20 +0100

    Clarify theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,20 @@ module.exports = {
       colors: {
         dw: {
           // drawer bg
-          500: "#1d1f25",
+          500: "#1d1f25", // 500
+          400: "#3e4554", // 400
+          300: "#4c5366", // 300
+          200: "#7e8baa", // 200
+          100: "#efeff3",
         },
         "dw-t-1": {
           // drawer text
-          500: "#efeff3",
         },
         "dw-t-2": {
           // drawer title & icon
-          500: "#4c5366",
         },
         "dw-left-icon": {
           // drawer left icon & <sm icon color
-          500: "#7e8baa",
         },
       },
     },
