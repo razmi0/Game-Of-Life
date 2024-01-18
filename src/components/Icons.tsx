4393d414167ca49ce13c838481e085082a04f3fb
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-18 20:42:40 +0100

    svg sprite icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,22 @@
+import spriteHref from "/sprite.svg?url";
+
+type IconNames = "dw-icon";
+type IconProps = {
+  width: number;
+  height: number;
+  name: IconNames;
+  color?: string;
+};
+/**
+ * <svg>
+ *      <defs>
+ *           <symbol id="dw-icon">
+ *               ...
+ */
+export default function Icon(props: IconProps) {
+  return (
+    <svg width={props.width + "px"} height={props.height + "px"}>
+      <use href={`${spriteHref}#${props.name}`} />
+    </svg>
+  );
+}
