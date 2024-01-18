7f79c7c55edf512e306ec0abdce1662acc3be777
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-01-18 17:26:18 +0100

    Add color them for drawer to twcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dw: {
+          // drawer bg
+          500: "#1d1f25",
+        },
+        "dw-t-1": {
+          // drawer text
+          500: "#efeff3",
+        },
+        "dw-t-2": {
+          // drawer title & icon
+          500: "#4c5366",
+        },
+      },
+    },
   },
   plugins: [],
 };
