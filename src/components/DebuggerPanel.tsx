de168aeb3aad86b8b741042de9d3bdfa429be333
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-02-04 03:36:01 +0100

    *

--- src/components/DebuggerPanel.tsx
+++ src/components/DebuggerPanel.tsx
@@ -11,8 +11,7 @@ const DebuggerPanel = (props: DebuggerPanelProps) => {
   return (
     <div class="absolute top-0 right-0 m-5 z-50">
       <div class=" py-1 px-2 flex flex-col bg-dw-300 w-fit max-w-64 opacity-80">
-        {/* <div class="h-fit w-full flex flex-row-reverse"> */}
-        <button class="hover:bg-dw-200  h-5 leading-tight w-full px-1" onClick={() => setOpen((p) => !p)}>
+        <button class="hover:bg-dw-200 h-5 leading-tight w-full px-1" onClick={() => setOpen((p) => !p)}>
           <Show when={open()} fallback={<>I</>}>
             <div class="flex flex-row justify-between ">
               <div>debug</div>
@@ -21,7 +20,6 @@ const DebuggerPanel = (props: DebuggerPanelProps) => {
             </div>
           </Show>
         </button>
-        {/* </div> */}
         <Show when={open()}>
           <Separator classes="bg-dw-500 w-full h-[2px] m-0" />
 
