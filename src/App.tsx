db80474e3e4de1ebf4e5f91ed317747ba59548bd
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-02-05 15:07:52 +0100

    Add hasStarted state and changeRandomization function

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,32 @@ import Drawer from "./components/Drawer";
 let canvas: HTMLCanvasElement;
 const App = () => {
   const [ctx, setCtx] = createSignal<CanvasRenderingContext2D>();
+  const [hasStarted, setHasStarted] = createSignal(false);
   const screen = useScreen(); // context candidate
   const data = useData();
   const { findColor } = useColors(screen.nCell());
   const { updateHash, readHashAndDraw, resetHash } = useHash(screen, data, findColor, ctx);
 
   const run = () => {
+    if (!hasStarted()) setHasStarted(true);
     updateHash();
     readHashAndDraw();
     data.incrementGeneration();
   };
 
   const reset = () => {
+    setHasStarted(false);
     resetHash();
     data.resetGeneration();
   };
 
+  const changeRandomization = (newRandom: number) => {
+    data.setRandom(newRandom);
+    reset();
+  };
+
   createEffect(() => {
-    if (screen.width) readHashAndDraw();
+    if (screen.width && hasStarted()) readHashAndDraw();
   });
 
   const { clock, changeSpeed } = useClock(run);
@@ -47,9 +55,21 @@ const App = () => {
           <SimpleButton handler={clock.switchPlayPause}>{clock.play ? "pause" : "play"}</SimpleButton>
         </DebuggerPanel>
       </Show>
-      <Drawer clock={clock} data={data} reset={reset} changeSpeed={changeSpeed} />
+      <Drawer
+        generation={data.generation}
+        nAlive={data.nAlive}
+        nDead={data.nDead}
+        randomness={data.randomness}
+        speed={clock.speed}
+        play={clock.play}
+        randomize={changeRandomization}
+        switchPlayPause={clock.switchPlayPause}
+        reset={reset}
+        changeSpeed={changeSpeed}
+        hasStarted={hasStarted()}
+      />
       <CanvasWrapper>
-        <canvas class="bg-slate-500" width={screen.width} height={screen.height} ref={canvas}></canvas>
+        <canvas class="bg-black" width={screen.width} height={screen.height} ref={canvas}></canvas>
       </CanvasWrapper>
     </>
   );
