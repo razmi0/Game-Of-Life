a37b6d41909574220c9c1065601a17ac4bdd3028
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-02-21 11:41:34 +0100

    Add getNavigatorInfo function to helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -7,3 +7,27 @@ export const debounce = (fn: Function, delay: number) => {
     }, delay);
   };
 };
+
+export async function getNavigatorInfo() {
+  let battery: null | number = null;
+
+  if ("getBattery" in navigator) {
+    const batteryInfo = await (navigator as any).getBattery();
+    battery = batteryInfo ? batteryInfo.level * 100 : null;
+  }
+
+  const platform = window.navigator.userAgentData.platform;
+  const userAgent = window.navigator.userAgent;
+  const hardwareConcurrency = window.navigator.hardwareConcurrency;
+  const deviceMemory = window.navigator.deviceMemory;
+  const availableThreads = hardwareConcurrency * deviceMemory;
+
+  return {
+    userAgent,
+    battery,
+    platform,
+    hardwareConcurrency,
+    deviceMemory,
+    availableThreads,
+  };
+}
