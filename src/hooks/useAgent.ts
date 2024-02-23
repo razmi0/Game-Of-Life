60d83e31e3d71befa68099a5948ae52b26da9dd8
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-02-23 10:34:28 +0100

    Add useAgent hook for retrieving user agent information

---
+++ src/hooks/useAgent.ts
@@ -0,0 +1,56 @@
+import { createSignal } from "solid-js";
+
+const userAgentInit: UserAgentInfo = {
+  userAgent: "",
+  battery: null,
+  batteryChange: 0,
+  platform: "",
+  hardwareConcurrency: 0,
+  deviceMemory: 0,
+  availableThreads: 0,
+};
+
+const useAgent = () => {
+  const [navInfo, setNavInfo] = createSignal<UserAgentInfo>(userAgentInit);
+
+  const getBatteryInfo = async () => {
+    let battery: null | number = null;
+    let startBattery: null | number = null;
+
+    if ("getBattery" in navigator) {
+      const batteryInfo = await (navigator as any).getBattery();
+      battery = batteryInfo ? batteryInfo.level * 100 : null;
+      if (startBattery === null) startBattery = battery;
+    }
+
+    console.log("battery : ", battery);
+    console.log("startBattery : ", startBattery);
+
+    setNavInfo((p) => {
+      if (p.battery !== battery && startBattery !== null && battery !== null) {
+        const batteryChange = battery - startBattery;
+        return { ...p, battery, batteryChange };
+      }
+      return { ...p, battery };
+    });
+  };
+
+  const getNavigatorInfo = () => {
+    //@ts-ignore
+    const platform = window.navigator.userAgentData.platform;
+    const userAgent = window.navigator.userAgent;
+    const hardwareConcurrency = window.navigator.hardwareConcurrency;
+    //@ts-ignore
+    const deviceMemory = window.navigator.deviceMemory;
+    const availableThreads = hardwareConcurrency * deviceMemory;
+
+    setNavInfo((prev) => ({ ...prev, userAgent, platform, hardwareConcurrency, deviceMemory, availableThreads }));
+  };
+
+  getNavigatorInfo();
+  getBatteryInfo();
+
+  return { navInfo, refreshBatteryInfo: getBatteryInfo };
+};
+
+export default useAgent;
