cea5fc709b5697463e12a4a59bfdef35d75ebd57
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-08 14:58:23 +0100

    Ui work

--- src/components/DebuggerPanel.tsx
+++ src/components/DebuggerPanel.tsx
@@ -1,5 +1,7 @@
 import { createSignal, Show, JSXElement } from "solid-js";
 import Separator from "./Drawer/Separator";
+import Icon from "./Icons";
+import { ICON_SIZE } from "../data/index";
 
 type DebuggerPanelProps = {
   children: JSXElement;
@@ -11,18 +13,17 @@ const DebuggerPanel = (props: DebuggerPanelProps) => {
   return (
     <div class="absolute top-0 right-0 m-5 z-50">
       <div class=" py-1 px-2 flex flex-col bg-dw-300 w-fit max-w-64 opacity-80">
-        <button class="hover:bg-dw-200 h-5 leading-tight w-full px-1" onClick={() => setOpen((p) => !p)}>
-          <Show when={open()} fallback={<>I</>}>
-            <div class="flex flex-row justify-between ">
-              <div>debug</div>
+        <button class="hover:bg-dw-200 leading-tight w-full px-1" onClick={() => setOpen((p) => !p)}>
+          <Show when={open()} fallback={<Icon name="plus_circle" width={ICON_SIZE.xl} />}>
+            <div class="flex flex-row justify-between items-center ">
+              <div>DEBUG</div>
               <Spacer />
-              <div>0</div>
+              <Icon name="minus_circle" width={ICON_SIZE.xl} />
             </div>
           </Show>
         </button>
         <Show when={open()}>
-          <Separator classes="bg-dw-500 w-full h-[2px] m-0" />
-
+          <Separator class="bg-dw-500 w-full h-[2px] m-0" />
           <div class="flex gap-1 flex-wrap">{props.children}</div>
         </Show>
       </div>
