cefa0372bd10657d56d0a7cc71acf8c11d4a626e
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-11 14:46:19 +0100

    Remove unused icons and add new component icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -15,8 +15,6 @@ export type IconNames =
   | "pause"
   | "speed"
   | "clock"
-  | "controls"
-  | "range"
   | "screen_gear"
   | "reset"
   | "minus_circle"
@@ -24,16 +22,13 @@ export type IconNames =
   | "pen"
   | "eraser"
   | "color_picker"
-  | "population"
   | "random"
-  | "queue"
   | "grid_square"
   | "two_by_two_squares"
   | "two_by_three_squares"
   | "arrowUp"
   | "pin"
   | "caret"
-  | "spacebar"
   | "arrowDown"
   | "snail"
   | "hare"
@@ -42,6 +37,12 @@ export type IconNames =
   | "square_shape"
   | "shape_picker"
   | "painting_tools";
+
+export type IconComponentNames = "minus_circle_FC" | "plus_circle_FC";
+
+// | "population"
+// | "spacebar"
+// | "queue"
 
 export type IconProps = {
   width: number;
@@ -65,3 +66,76 @@ export default function Icon(props: IconProps) {
     </svg>
   );
 }
+
+export type OpCircleIconProps = {
+  width: number;
+  height?: number;
+  color?: string;
+  class?: string;
+  classList?: Record<string, boolean | undefined>;
+  style?: string;
+};
+
+export const MinusCircleIcon = (props: OpCircleIconProps) => {
+  const color = () => props.color || "#b3b3b7";
+  const classNames = props.class || "";
+  const style = props.style || "";
+  const height = props.height || props.width;
+  return (
+    <svg
+      width={props.width + "px"}
+      height={height + "px"}
+      class={classNames}
+      classList={props.classList || {}}
+      style={style}
+      id="minus_circle"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M12 2.84952C17.0536 2.84952 21.1504 6.94633 21.1504 12C21.1504 17.0537 17.0536 21.1505 12 21.1505C6.9463 21.1505 2.84949 17.0537 2.84949 12C2.84949 6.94633 6.9463 2.84952 12 2.84952Z"
+          stroke={color()}
+          stroke-width="1.69904"
+        ></path>
+        <path d="M9.39941 12L14.5902 12" stroke={color()} stroke-width="1.69904" stroke-linecap="round"></path>
+      </g>
+    </svg>
+  );
+};
+
+export const PlusCircleIcon = (props: OpCircleIconProps) => {
+  const color = () => props.color || "#b3b3b7";
+  const classNames = props.class || "";
+  const style = props.style || "";
+  const height = props.height || props.width;
+
+  return (
+    <svg
+      width={props.width + "px"}
+      height={height + "px"}
+      class={classNames}
+      classList={props.classList || {}}
+      style={style}
+      id="plus_circle"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M12 2.84952C17.0536 2.84952 21.1504 6.94633 21.1504 12C21.1504 17.0537 17.0536 21.1505 12 21.1505C6.9463 21.1505 2.84949 17.0537 2.84949 12C2.84949 6.94633 6.9463 2.84952 12 2.84952Z"
+          stroke={color()}
+          stroke-width="1.69904"
+        ></path>
+        <path d="M9.39941 12L14.5902 12" stroke={color()} stroke-width="1.69904" stroke-linecap="round"></path>
+        <path d="M11.9949 9.4046L11.9949 14.5954" stroke={color()} stroke-width="1.69904" stroke-linecap="round"></path>
+      </g>
+    </svg>
+  );
+};
