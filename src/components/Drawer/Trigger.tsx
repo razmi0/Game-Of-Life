348a80371eabbc4e3bb6f0b341be8c7c4b9d7988
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-12 14:58:34 +0100

    Refactor Trigger component  for reactive onClick event

--- src/components/Drawer/Trigger.tsx
+++ src/components/Drawer/Trigger.tsx
@@ -8,7 +8,7 @@ type TriggerProps = {
 const Trigger: VoidComponent<TriggerProps> = (props) => {
   return (
     <div class="absolute top-0 m-5 z-10">
-      <button class="w-fit p-1 backdrop-blur-sm bg-dw-500 rounded-lg" onClick={props.trigger}>
+      <button class="w-fit p-1 backdrop-blur-sm bg-dw-500 rounded-lg" onClick={() => props.trigger()}>
         <Icon width={50} height={50} name="chevron" />
       </button>
     </div>
