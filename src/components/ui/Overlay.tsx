f57ecb82afb477841a03eb02f8b2f7839a155ecb
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-03-12 14:55:13 +0100

    Renamed Wrapper.tsx to Overlay.tsx component

--- src/components/ui/Overlay.tsx
+++ src/components/ui/Overlay.tsx
@@ -1,5 +1,5 @@
 import type { VoidComponent } from "solid-js";
 
 export const Overlay: VoidComponent = () => {
-  return <div class="backdrop-blur-sm bg-white/10 w-full h-full"></div>;
+  return <div class="backdrop-blur-sm bg-white/10 w-full h-full" />;
 };
